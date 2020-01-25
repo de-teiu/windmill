f0a19b7ee7fe8dac433818819d748de1554060b7
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2020-01-25 15:48:02 +0900

    getUserMediaの取り方修正

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const BUFFER_SIZE = 1024;
 const wingDom = document.getElementById("wing");
 
 window.onload = () => {
-  document.getElementById("btnStart").addEventListener("click",(e)=>{
+  document.getElementById("btnStart").addEventListener("click", (e) => {
     const entranceDom = document.getElementById("entrance");
     entranceDom.parentNode.removeChild(entranceDom);
     activateMicrophone();
@@ -16,47 +16,45 @@ window.onload = () => {
  */
 const activateMicrophone = () => {
   console.log("activateMicrophone");
-  navigator.getUserMedia({audio: true,video:false},(stream) => {
-    console.log("getUserMedia");
-    const audioContext = new(window.AudioContext || window.webkitAudioContext)();
-    const audioAnalyser = audioContext.createAnalyser();
-    const mediaStreamSource = audioContext.createMediaStreamSource(stream);
-    audioAnalyser.fftSize = 256;
-    mediaStreamSource.connect(audioAnalyser);
-    
-    const scriptProcessor = audioContext.createScriptProcessor(BUFFER_SIZE, 1, 1);
-    audioAnalyser.connect(scriptProcessor);
-    scriptProcessor.connect(audioContext.destination);
+  console.log(navigator.mediaDevices);
+  navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: false
+  }).then(
+    stream => {
+      console.log("getUserMedia");
+      const audioContext = new(window.AudioContext || window.webkitAudioContext);
+      const audioAnalyser = audioContext.createAnalyser();
+      const mediaStreamSource = audioContext.createMediaStreamSource(stream);
+      audioAnalyser.fftSize = 256;
+      mediaStreamSource.connect(audioAnalyser);
 
-    //音声入力検知イベント
-    scriptProcessor.onaudioprocess = (e) => {
-      console.log("analyseAudio");
-      const spectrums = new Uint8Array(audioAnalyser.frequencyBinCount);
-      audioAnalyser.getByteFrequencyData(spectrums);
+      const scriptProcessor = audioContext.createScriptProcessor(BUFFER_SIZE, 1, 1);
+      audioAnalyser.connect(scriptProcessor);
+      scriptProcessor.connect(audioContext.destination);
 
-      const isRotating = wingDom.classList.contains("animation");
-      const spc = spectrums[0];
-      console.log(spc);
-      if (spc > 200 && !isRotating) {
-        wingDom.classList.remove("animation-willstop");
-        wingDom.classList.add("animation");
-      } else if (spc < 100 && isRotating) {
-        wingDom.classList.remove("animation");
-        wingDom.classList.add("animation-willstop");
-      }
-      console.log("analyseAudio - 完了");
-    };
-    
+      //音声入力検知イベント
+      scriptProcessor.onaudioprocess = (e) => {
+        console.log("analyseAudio");
+        const spectrums = new Uint8Array(audioAnalyser.frequencyBinCount);
+        audioAnalyser.getByteFrequencyData(spectrums);
 
-    // 音声解析関連
-    /*
-    
-    frequencyData = new Uint8Array(audioAnalyser.frequencyBinCount);
-    timeDomainData = new Uint8Array(audioAnalyser.frequencyBinCount);
-    
-    */
-    console.log("getUserMedia - 完了");
-  },(error) =>{
+        const isRotating = wingDom.classList.contains("animation");
+        const spc = spectrums[0];
+        console.log(spc);
+        if (spc > 200 && !isRotating) {
+          wingDom.classList.remove("animation-willstop");
+          wingDom.classList.add("animation");
+        } else if (spc < 100 && isRotating) {
+          wingDom.classList.remove("animation");
+          wingDom.classList.add("animation-willstop");
+        }
+        console.log("analyseAudio - 完了");
+      };
+      console.log("getUserMedia - 完了");
+    }
+
+  ).catch(error => {
     console.log(error);
   });
 };
