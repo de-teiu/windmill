ddb3246536f4d5302c2731885f6013c0aef4c65e
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2020-01-25 17:05:05 +0900

    デバッグログの削除など

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const BUFFER_SIZE = 1024;
-
-const wingDom = document.getElementById("wing");
-//const valueDom = document.getElementById("value");
-
+/**
+ * 初期化処理
+ */
 window.onload = () => {
+  /**
+   * 初期画面タップイベント
+   */
   document.getElementById("btnStart").addEventListener("click", (e) => {
     const entranceDom = document.getElementById("entrance");
     entranceDom.parentNode.removeChild(entranceDom);
+    //マイク起動
     activateMicrophone();
   });
 };
@@ -15,14 +17,14 @@ window.onload = () => {
  * マイク起動
  */
 const activateMicrophone = () => {
-  console.log("activateMicrophone");
-  console.log(navigator.mediaDevices);
+  const wingDom = document.getElementById("wing");
+  const BUFFER_SIZE = 1024;
+
   navigator.mediaDevices.getUserMedia({
     audio: true,
     video: false
   }).then(
     stream => {
-      console.log("getUserMedia");
       const audioContext = new(window.AudioContext || window.webkitAudioContext);
       const audioAnalyser = audioContext.createAnalyser();
       const mediaStreamSource = audioContext.createMediaStreamSource(stream);
@@ -35,14 +37,11 @@ const activateMicrophone = () => {
 
       //音声入力検知イベント
       scriptProcessor.onaudioprocess = (e) => {
-        console.log("analyseAudio");
         const spectrums = new Uint8Array(audioAnalyser.frequencyBinCount);
         audioAnalyser.getByteFrequencyData(spectrums);
 
         const isRotating = wingDom.classList.contains("animation");
         const spc = spectrums[0];
-        console.log(spc);
-        //valueDom.innerHTML = spc;
         if (spc > 200 && !isRotating) {
           wingDom.classList.remove("animation-willstop");
           wingDom.classList.add("animation");
@@ -50,9 +49,7 @@ const activateMicrophone = () => {
           wingDom.classList.remove("animation");
           wingDom.classList.add("animation-willstop");
         }
-        console.log("analyseAudio - 完了");
       };
-      console.log("getUserMedia - 完了");
     }
 
   ).catch(error => {
